ea63afca11e52a89ddb9df02f1295cd28565c58d
William M <cominatyou@cominatyou.com>
2023-06-20 19:17:02 -0500

    Add activity for editing tile text

---
+++ app/src/main/java/com/cominatyou/batterytile/preferences/EditTileTextActivity.java
@@ -0,0 +1,72 @@
+package com.cominatyou.batterytile.preferences;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+
+import com.cominatyou.batterytile.R;
+import com.cominatyou.batterytile.databinding.ActivityEditTileTextBinding;
+import com.google.android.material.color.DynamicColors;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Objects;
+
+public class EditTileTextActivity extends AppCompatActivity {
+    private boolean hasTextBeenChanged = false;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DynamicColors.applyToActivityIfAvailable(this);
+        final ActivityEditTileTextBinding binding = ActivityEditTileTextBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        final String preference_key = getIntent().getBooleanExtra("isEditingChargingText", false) ? "chargingText" : "dischargingText";
+
+        binding.editTileTextEditText.setText(getSharedPreferences("preferences", MODE_PRIVATE).getString(preference_key, ""));
+
+        binding.editTileTextEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                hasTextBeenChanged = true;
+            }
+            public void beforeTextChanged(CharSequence s, int a, int b, int c) {}
+            public void afterTextChanged(Editable s) {}
+        });
+
+        binding.topBar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.save_menu_item) {
+                getSharedPreferences("preferences", MODE_PRIVATE).edit().putString(preference_key, Objects.requireNonNull(binding.editTileTextEditText.getText()).toString()).apply();
+                hasTextBeenChanged = false;
+                Snackbar.make(binding.getRoot(), R.string.activity_edit_tile_text_save_success, Snackbar.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        });
+
+        binding.topBar.setNavigationOnClickListener(v -> warnForUnsavedChanges());
+    }
+
+    @Override
+    public void onBackPressed() {
+        warnForUnsavedChanges();
+    }
+
+    private void warnForUnsavedChanges() {
+        if (hasTextBeenChanged) {
+            new MaterialAlertDialogBuilder(this)
+                    .setTitle(R.string.activity_edit_text_discard_dialog_title)
+                    .setMessage(R.string.activity_edit_text_discard_dialog_description)
+                    .setNegativeButton(R.string.activity_edit_text_discard_dialog_negative_button, (dialog, which) -> finish())
+                    .setPositiveButton(R.string.activity_edit_text_discard_dialog_positive_button, (dialog, which) -> dialog.dismiss())
+                    .show();
+        }
+        else {
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/cominatyou/batterytile/preferences/TileTextDisambiguationDialog.java
+++ app/src/main/java/com/cominatyou/batterytile/preferences/TileTextDisambiguationDialog.java
@@ -1,6 +1,7 @@
 package com.cominatyou.batterytile.preferences;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.Gravity;
 import android.widget.TextView;
 
@@ -22,7 +23,13 @@ public class TileTextDisambiguationDialog {
                 .setSingleChoiceItems(new String[] { context.getString(R.string.bottom_sheet_tile_text_disambiguation_charging_text_title), context.getString(R.string.bottom_sheet_tile_text_disambiguation_discharging_text_title) }, 0, (dialog, which) -> choice[0] = which)
                 .setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.dismiss())
                 .setPositiveButton(android.R.string.ok, (dialog, which) -> {
-                    // TODO: display tile text activity
+                    if (choice[0] == 0) {
+                        context.startActivity(new Intent(context, EditTileTextActivity.class).putExtra("isEditingChargingText", true));
+                    }
+                    else {
+                        context.startActivity(new Intent(context, EditTileTextActivity.class).putExtra("isEditingChargingText", false));
+                    }
+
                     dialog.dismiss();
                 })
                 .setCustomTitle(title)
