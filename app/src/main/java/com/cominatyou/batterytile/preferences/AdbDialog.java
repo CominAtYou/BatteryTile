428dd6c3688ac8186a474b35ba138605473a3c0c
William M <cominatyou@cominatyou.com>
2024-02-14 15:55:17 -0600

    Use lambda for onClick callback

--- app/src/main/java/com/cominatyou/batterytile/preferences/AdbDialog.java
+++ app/src/main/java/com/cominatyou/batterytile/preferences/AdbDialog.java
@@ -1,11 +1,9 @@
 package com.cominatyou.batterytile.preferences;
 
+import android.Manifest;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-
-import android.Manifest;
-import android.content.DialogInterface;
 import android.text.Html;
 import android.widget.Toast;
 
@@ -19,14 +17,11 @@ public class AdbDialog {
                 .setMessage(Html.fromHtml(context.getString(R.string.requires_adb_dialog_description) + "<br><br><tt>adb shell pm grant " + context.getPackageName() + " " + Manifest.permission.WRITE_SECURE_SETTINGS +
                         "</tt><br><br>" + context.getString(R.string.requires_adb_dialog_description_second_half), Html.FROM_HTML_MODE_COMPACT))
                 .setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss())
-                .setNeutralButton(R.string.requires_adb_dialog_copy_button, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-                        ClipData clip = ClipData.newPlainText("", "adb shell pm grant " + context.getPackageName()+ " " + Manifest.permission.WRITE_SECURE_SETTINGS);
-                        clipboard.setPrimaryClip(clip);
-                        Toast.makeText(context, R.string.requires_adb_dialog_copy_success, Toast.LENGTH_LONG).show();
-                    }
+                .setNeutralButton(R.string.requires_adb_dialog_copy_button, (dialog, which) -> {
+                    ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("", "adb shell pm grant " + context.getPackageName()+ " " + Manifest.permission.WRITE_SECURE_SETTINGS);
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(context, R.string.requires_adb_dialog_copy_success, Toast.LENGTH_LONG).show();
                 })
                 .show();
     }
