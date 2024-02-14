059902e3438bbd090b57f825bf7e4bd3b91f0b8c
thevllad <58750054+thevllad@users.noreply.github.com>
2024-02-14 20:54:56 +0200

    Added a copy button in the ADB dialog
    Added a button to copy an ADB command for further use

--- app/src/main/java/com/cominatyou/batterytile/preferences/AdbDialog.java
+++ app/src/main/java/com/cominatyou/batterytile/preferences/AdbDialog.java
@@ -1,9 +1,13 @@
 package com.cominatyou.batterytile.preferences;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.text.Html;
+import android.widget.Toast;
 
 import com.cominatyou.batterytile.R;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -15,6 +19,15 @@ public class AdbDialog {
                 .setMessage(Html.fromHtml(context.getString(R.string.requires_adb_dialog_description) + "<br><br><tt>adb shell pm grant " + context.getPackageName() + " " + Manifest.permission.WRITE_SECURE_SETTINGS +
                         "</tt><br><br>" + context.getString(R.string.requires_adb_dialog_description_second_half), Html.FROM_HTML_MODE_COMPACT))
                 .setPositiveButton(android.R.string.ok, (dialog, which) -> dialog.dismiss())
+                .setNeutralButton(R.string.requires_adb_dialog_copy_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                        ClipData clip = ClipData.newPlainText("", "adb shell pm grant " + context.getPackageName()+ " " + Manifest.permission.WRITE_SECURE_SETTINGS);
+                        clipboard.setPrimaryClip(clip);
+                        Toast.makeText(context, R.string.requires_adb_dialog_copy_success, Toast.LENGTH_LONG).show();
+                    }
+                })
                 .show();
     }
 }
