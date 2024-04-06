580acd10efc29507caad0f5f08a29b5989e396f0
William M. <cominatyou@cominatyou.com>
2024-04-06 13:15:25 -0500

    Add wattage formatter

--- app/src/main/java/com/cominatyou/batterytile/preferences/TileTextFormatter.java
+++ app/src/main/java/com/cominatyou/batterytile/preferences/TileTextFormatter.java
@@ -18,17 +18,21 @@ public class TileTextFormatter {
     public TileTextFormatter(Context context) {
         final BatteryManager bm = context.getSystemService(BatteryManager.class);
         final Intent batteryIntent = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        final DecimalFormat numberFormat = new DecimalFormat("#.##");
         assert batteryIntent != null;
+
+        final int instantaneousCurrent = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW);
+        final int voltage = batteryIntent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0); // provided as mV
 
         // CURRENT_NOW and CURRENT_AVERAGE are positive when charging and negative when discharging
         if (Build.MANUFACTURER.equalsIgnoreCase("samsung")) {
             // Samsung devices, for some reason, returns this in milli-amperes instead of microamperes like the Android documentation specifies
-            formatters.put("c", bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW));
+            formatters.put("c", instantaneousCurrent);
             formatters.put("a", bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_AVERAGE));
         }
         else {
             // Android provides these as microamperes, so divide by 1000 to convert to mA
-            formatters.put("c", bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW) / 1000);
+            formatters.put("c", instantaneousCurrent / 1000);
             formatters.put("a", bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_AVERAGE) / 1000);
         }
 
@@ -37,7 +41,10 @@ public class TileTextFormatter {
         formatters.put("f", (int) (batteryIntent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10 * 1.8 + 32)); // Fahrenheit
 
         // Voltage of the battery - provided as mV by Android, so divide by 1000 to get volts
-        formatters.put("v", new DecimalFormat("#.##").format(batteryIntent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000f));
+        formatters.put("v", numberFormat.format(voltage / 1000f));
+
+        // Wattage of the battery - Power = I * V (divide instantaneousCurrent by 1,000,000 to get amps)
+        formatters.put("w", numberFormat.format(Math.abs(instantaneousCurrent / 1000000f * voltage / 1000f)));
 
         // Battery level
         formatters.put("l", batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0));
